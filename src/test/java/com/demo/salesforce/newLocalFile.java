bf62f0572f7b6be8761b73affa98274b1a0c8afd
Ravi <imrshankar27@gmail.com>
2021-03-01 19:34:40 +0530

    new commit

--- src/test/java/com/demo/salesforce/newLocalFile.java
+++ src/test/java/com/demo/salesforce/newLocalFile.java
@@ -1,10 +1,9 @@
 package com.demo.salesforce;
 
-public class newcommit {
+public class newLocalFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("local file");
 
 	}
 
